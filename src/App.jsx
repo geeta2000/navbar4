11d12007df98e8fa043850fd8b0686f686f9ef50
geeta2000 <giranchipatel22@gmail.com>
2024-05-03 13:19:44 +0530

    inportent.main

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      this is Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+      facere est cumque saepe ad iusto dicta, impedit porro similique quos
+      aliquam. Beatae totam vel, deserunt consequuntur dolorem dolorum minus
+      optio!
+    </div>
+  );
+}
+
+export default App;
